aa01bbe36fc15d1a68fb5366892415ad1bd1e09e
Durvesh725 <durveshjagtap45@gmail.com>
2023-11-01 19:33:15 +0530

    Madhu chi madat

--- base/forms.py
+++ base/forms.py
@@ -1,15 +1,44 @@
-# from django import forms
-# from .models import steganography
-
-# class SteganographyForm(forms.ModelForm):
-#     class Meta:
-#         model = steganography
-#         fields = ['image', 'message', 'password']
-
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 class EncodeImageForm(forms.Form):
     image = forms.ImageField()
     message = forms.CharField(widget=forms.Textarea)
     password = forms.CharField(max_length=128, widget=forms.PasswordInput)
     dest = forms.CharField(initial='encoded_image.png')
+    receiver = forms.ModelChoiceField(queryset=User.objects.all(), label = 'Select Receiver')
+
+
+class DecodeImageForm(forms.Form):
+    encoded_image = forms.ImageField(label='Select the image to decode: ')
+    password = forms.CharField(max_length=128, widget=forms.PasswordInput)
+
+
+class RegistrationForm(UserCreationForm):
+    profile_picture = forms.ImageField(required=False, label='Profile Picture')
+    email = forms.CharField(label='Email')
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'custom-username'})
+        self.fields['password1'].widget.attrs.update({'class': 'custom-password1'})
+        self.fields['password2'].widget.attrs.update({'class': 'custom-password2'})
+        self.fields['email'].widget.attrs.update({'class': 'custom-email'})
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'profile_picture']
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, label='Username')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'custom-username'})
+        self.fields['password'].widget.attrs.update({'class': 'custom-password1'})
+
+
--- securePixel/settings.py
+++ securePixel/settings.py
@@ -126,9 +126,11 @@ STATICFILES_DIRS = [
 ]
 
 # user uploaded content
-MEDIA_ROOT = BASE_DIR / 'static/stegoImages'
+MEDIA_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'home'
